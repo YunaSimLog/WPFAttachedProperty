ab52f0796bc381b039095ef7d59edd2fcf612da6
yunasim <ynsim@solidit.co.kr>
2024-02-05 17:55:08 +0900

    #✨feature: 기본 UI 작성 및 바인딩 데이터 연결

---
+++ WPFAttachedProperty/ModelViews/MainViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfBase;
+
+namespace WPFAttachedProperty.ModelViews
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private string _inputText;
+        private string _outputText;
+        private string _inputPassword;
+        private string _outputPassword;
+
+        public string InputText
+        {
+            get => _inputText;
+            set
+            {
+                _inputText = value;
+                OnPropertyChanged();
+                OutputText = _inputText;
+                InputPassword = _inputText;
+            }
+        }
+
+        public string OutputText
+        {
+            get => _outputText;
+            set
+            {
+                _outputText = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string InputPassword
+        {
+            get => _inputPassword; set
+            {
+                _inputPassword = value;
+                OnPropertyChanged();
+                OutputPassword = _inputPassword;
+            }
+        }
+
+        public string OutputPassword
+        {
+            get => _outputPassword;
+            set
+            {
+                _outputPassword = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
