ab3b36e7cb55959fd6eae82be57e691b7a289723
yunasim <ynsim@solidit.co.kr>
2024-02-05 18:26:42 +0900

    #😎update: TextBox Attached Property 작성하여 연결

---
+++ WPFAttachedProperty/Utils/TextBoxHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace WPFAttachedProperty.Utils
+{
+    public class TextBoxHelper
+    {
+        private static void OnUseOnPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var texBox = d as TextBox;
+            if (texBox == null)
+                return;
+            if ((bool)e.NewValue)
+            {
+                texBox.TextChanged += TextBox_TextChanged;
+            }
+            else
+            {
+                texBox.TextChanged -= TextBox_TextChanged;
+            }
+        }
+
+        private static void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            BindingExpression bindingExpression = textBox.GetBindingExpression(TextBox.TextProperty);
+            bindingExpression?.UpdateSource();
+        }
+
+        public static readonly DependencyProperty UseOnPropertyChangedProperty = DependencyProperty.RegisterAttached("UseOnPropertyChanged",
+            typeof(bool), typeof(TextBoxHelper), new PropertyMetadata(false, OnUseOnPropertyChanged));
+
+        public static bool GetUseOnPropertyChanged(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(UseOnPropertyChangedProperty);
+        }
+
+        public static void SetUseOnPropertyChanged(DependencyObject obj, bool value)
+        {
+            obj.SetValue(UseOnPropertyChangedProperty, value);
+        }
+    }
+}
